6eab9194fe3f0bff886dc68449f4eb7a31700f3d
michael <michael@peoplelikesoftware.com>
2024-02-08 11:54:34 -0500

    switched admin theme

--- resources/views/layouts/head-css.blade.php
+++
@@ -1,5 +0,0 @@
-@yield('css')
-<link rel="stylesheet" href="{{ URL::asset('build/css/icons.min.css') }}" />
-<link rel="stylesheet" href="{{ URL::asset('build/css/tailwind.min.css') }}" />
-
-
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'root']);
-    Route::get('{any}', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+Route::get('/', function () {
+    return view('layout.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
