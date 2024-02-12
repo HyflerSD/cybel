f8fb92fadce30b1d2c41fe6d813555ecd53a79e3
michael <michael@peoplelikesoftware.com>
2024-02-11 23:02:56 -0500

    removed todo comments

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Student extends Model
 {
@@ -18,12 +19,16 @@ class Student extends Model
 
     protected $hidden = ['password'];
 
-    //@todo Build advisor model
+    /**
+     * @return BelongsTo
+     */
     public function advisor() {
         return $this->belongsTo(Advisor::class, 'advisorID', 'advisorID');
     }
 
-    //@todo build campus model
+    /**
+     * @return BelongsTo
+     */
     public function campus() {
         return $this->belongsTo(Campus::class, 'campusID', 'campusID');
     }
