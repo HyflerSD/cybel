927ffa5296857d85917f968ec3306015645abbbd
Rolando Caceres <rolycaceres@gmail.com>
2024-03-03 20:33:37 -0500

    updated the frontend of our login page and also started on the signup frontend to match the login.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <!-- Sign up form -->
-    <section class="signup">
-        <div class="container">
-            <div class="signup-content">
-                <div class="signup-form">
-                    <h2 class="form-title">Sign up</h2>
+    <section class="signup" style=" background: radial-gradient(circle, rgba(42,133,154,1) 0%, rgba(73,19,166,1) 72%, rgba(50,0,176,1) 82%, rgba(53,0,176,1) 99%);;position: fixed; top: 0; left: 0; width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center;">
+        <div class="container" style="background-color: rgba(0, 0, 0, 0.5); padding: 20px; border-radius: 100px;">
+            <div class="signup-content" style="display: flex; align-items: center;">
+                <div class="signup-form" style="width: 100%;">
+                    <h2 class="form-title" style="text-align: center;">Sign up</h2>
                     <form method="POST" class="register-form" action="{{ route('register') }}" id="register-form">
                         @csrf
                         <div class="form-group">
@@ -64,18 +64,19 @@
                                        class="form-control input-height" /> </div>
                         </div>
                         <div class="form-group">
-                            <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
-                            <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all
-                                statements in <a href="#" class="term-service">Terms of service</a></label>
+                            <label for="agree-term" class="label-agree-term">
+                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                                <span><span></span></span>I agree to <a href="#" class="term-service">Terms of service</a>
+                            </label>
                         </div>
-                        <div class="form-group form-button">
-                            <button class="btn btn-round btn-primary" name="signup" id="register">Register</button>
+                        <div class="form-group form-button" style="display: flex; justify-content: center; align-items: center; margin-top: 20px;">
+                            <button class="btn btn-round" name="signup" id="register" style="background-color: royalblue; color: white;" onmouseover="this.style.backgroundColor='navy'; this.style.color='white';" onmouseout="this.style.backgroundColor='royalblue'; this.style.color='white';">Register</button>
                         </div>
                     </form>
                 </div>
                 <div class="signup-image">
-                    <figure><img src="/admin/assets/img/pages/signup.jpg" alt="sing up image"></figure>
-                    <a href="{{ route('login') }}" class="signup-image-link">I am already member</a>
+                    <figure><img src="/admin/assets/img/pages/poweredbyCybel.png" alt="sing up image"></figure>
+                    <a href="{{ route('login') }}" class="signup-image-link">I am already member.</a>
                 </div>
             </div>
         </div>
