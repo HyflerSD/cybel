28ca9f8948f57eb5fd262658ffbc757b41a803c3
Sueda Tare <sueda.tare001@mymdc.net>
2024-03-09 18:03:52 -0500

    updated student dashboard,added profile page,updated student service,update the student model,added a new route

--- routes/web.php
+++ routes/web.php
@@ -1,51 +1,193 @@
-<?php
+@extends('layouts.admin')
 
-use App\Http\Controllers\Admin\ProfessorsController;
-use App\Http\Controllers\Advisor\AdviseesController;
-use App\Http\Controllers\CoursesController;
-use App\Http\Controllers\StudentController;
-use App\Http\Middleware\IsStudentUser;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\IsAdminUser;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+@section('title') {{'Student Dashboard'}} @endsection
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('auth');
+@section('content')
+    <!-- start page content -->
+    <div class="page-content-wrapper">
+        <div class="page-content">
+            <div class="page-bar">
+            </div>
+            <!-- start widget -->
+            <div class="row ">
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card comp-card">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <div class="col mt-0">
+                                        <h4 class="info-box-title">Courses This Semester</h4>
+                                    </div>
+                                    <h3 class="mt-1 mb-3 info-box-title col-green">4</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card comp-card">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <div class="col mt-0">
+                                        <h4 class="info-box-title">GPA</h4>
+                                    </div>
+                                    <h3 class="mt-1 mb-3 info-box-title col-green">
+                                         {{ session('student')->gpa }}
+                                    </h3>
 
-Route::group(['prefix' => 'admin', 'middleware' => IsAdminUser::class], function (){
-    Route::get('/dashboard', [App\Http\Controllers\AdvisorsController::class, 'index'])->name('admin.dashboard');
-    Route::group(['prefix' => 'professors'], function (){
-        Route::get('/', [ProfessorsController::class, 'index'])->name('admin.professors');
-    });
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card comp-card">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <div class="col mt-0">
+                                        <h4 class="info-box-title">Major</h4>
+                                    </div>
+                                    <h3 class="mt-1 mb-3 info-box-title col-green">{{ session('student')->concentration->name }}</h3>
 
-    Route::group(['prefix' => 'courses'], function () {
-        Route::get('/', [\App\Http\Controllers\Advisor\CoursesController::class, 'index'])->name('admin.courses');
-    });
+                                </div>
+                                <div class="col-auto">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card comp-card">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <div class="col mt-0">
+                                        <h4 class="info-box-title">Total credits</h4>
+                                    </div>
+                                    <h3 class="mt-1 mb-3 info-box-title col-green">{{ session('student')->total_credits_earned }}</h3>
 
-    Route::group(['prefix' => 'advisees'], function () {
-        Route::get('/', [AdviseesController::class, 'index'])->name('admin.advisees');
-    });
-});
-
-Route::group(['prefix' => 'student', 'middleware' => IsStudentUser::class], function (){
-    Route::get('/student-dashboard', [App\Http\Controllers\StudentController::class, 'index'])->name('student.dashboard');
-    Route::group(['prefix' => 'courses'], function (){
-        Route::get('/', [CoursesController::class, 'index'])->name('student.courses');
-    });
-    Route::group(['prefix' => 'profile'], function (){
-        Route::get('/', [StudentController::class, 'profile'])->name('student.profile');
-    });
-});
-Auth::routes();
-
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end widget -->
+            <!-- chart start -->
+            <div class="row">
+                <div class="col-12 col-sm-12 col-lg-6">
+                    <div class="card">
+                        <div class="card-head">
+                            <header>Chart</header>
+                        </div>
+                        <div class="card-body">
+                            <div id="chart2"></div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <p class="text-muted font-15 text-truncate">Target</p>
+                                    <h5>
+                                        <i class="fa fa-arrow-circle-up col-green ms-1 me-1"></i>$15.3k
+                                    </h5>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted font-15 text-truncate">Last
+                                        week</p>
+                                    <h5>
+                                        <i class="fa fa-arrow-circle-down col-red ms-1 me-1"></i>$2.8k
+                                    </h5>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted text-truncate">Last
+                                        Month</p>
+                                    <h5>
+                                        <i class="fa fa-arrow-circle-up col-green ms-1 me-1"></i>$12.5k
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-lg-6">
+                    <div class="card">
+                        <div class="card-head">
+                            <header>Chart</header>
+                        </div>
+                        <div class="card-body">
+                            <div id="schart3"></div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <p class="text-muted font-15 text-truncate">Target</p>
+                                    <h5>
+                                        <i class="fa fa-arrow-circle-up col-green ms-1 me-1"></i>$15.3k
+                                    </h5>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted font-15 text-truncate">Last
+                                        week</p>
+                                    <h5>
+                                        <i class="fa fa-arrow-circle-down col-red ms-1 me-1"></i>$2.8k
+                                    </h5>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted text-truncate">Last
+                                        Month</p>
+                                    <h5>
+                                        <i class="fa fa-arrow-circle-up col-green ms-1 me-1"></i>$12.5k
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- start new student list -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-head">
+                            <header>Current Courses</header>
+                            <div class="tools">
+                                <a class="fa fa-repeat btn-color box-refresh" href="javascript:;"></a>
+                                <a class="t-collapse btn-color fa fa-chevron-down" href="javascript:;"></a>
+                                <a class="t-close btn-color fa fa-times" href="javascript:;"></a>
+                            </div>
+                        </div>
+                        <div class="card-body ">
+                            <table id="saveStage" class="display" style="width:100%;">
+                                <thead>
+                                <tr>
+                                    <th>Code</th>
+                                    <th>Name</th>
+                                    <th>Credits</th>
+                                    <th>Type</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>Tiger Nixon</td>
+                                    <td>System Architect</td>
+                                    <td>Edinburgh</td>
+                                    <td>Edinburgh</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end new student list -->
+        </div>
+    </div>
+    <!-- end page content -->
+@endsection
+@section('styles')
+    <link href="/admin/assets/plugins/datatables/plugins/bootstrap/dataTables.bootstrap5.min.css" rel="stylesheet"
+          type="text/css" />
+@endsection
+@section('scripts')
+{{--    <script src="/admin/assets/plugins/datatables/jquery.dataTables.min.js"></script>--}}
+    <script src="/admin/assets/plugins/datatables/plugins/bootstrap/dataTables.bootstrap5.min.js"></script>
+    <script src="/admin/assets/js/pages/table/table_data.js"></script>
+@endsection
