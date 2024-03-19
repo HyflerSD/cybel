f79478b3f56e39cd427e5ef8a915f0c4e293d24b
michael <michael@peoplelikesoftware.com>
2024-03-19 10:37:14 -0400

    added more structure

--- resources/views/admin/models.blade.php
+++ resources/views/admin/models.blade.php
@@ -15,22 +15,13 @@
                         <div class="col-md-12">
                             <div class="card">
                                 <div class="card-head">
-                                    <header>{{ 'Concentration Code: S9501 | Effective Data: 09-22-2029' }}</header>
+                                    <header>{{ 'Concentration Code: S9501 | Effective Date: 09-22-2029' }}</header>
                                     <div class="tools">
                                         <a class="fa fa-repeat btn-color box-refresh" href="javascript:;"></a>
                                         <a class="t-collapse btn-color fa fa-chevron-down" href="javascript:;"></a>
                                     </div>
                                 </div>
                                 <div class="card-body ">
-                                    <div class="row">
-                                        <div class="col-md-6 col-sm-6 col-6">
-                                            <div class="btn-group">
-                                                <button id="add-button" class="btn btn-info">
-                                                    Add To Model <i class="fa fa-plus"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
                                     <table
                                         class="table table-striped table-bordered table-hover table-checkable order-column"
                                         style="width: 100%" id="example4">
