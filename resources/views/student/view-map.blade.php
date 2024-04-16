ec31f9c8e4635b33786ec46968a100510883c508
Rolando Caceres <rolycaceres@gmail.com>
2024-04-16 16:23:12 -0400

    Made view map clickable from the students frontend and now displays the students semester cards

---
+++ resources/views/student/view-map.blade.php
@@ -0,0 +1,220 @@
+@extends('layouts.admin')
+@section('title') {{'Degree Models'}} @endsection
+@section('content')
+
+
+<div class="page-content-wrapper">
+    <div class="page-content">
+        <div class="page-bar">
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+                <div class="card  card-box">
+                    <div class="card-head">
+                        <header>Summer 2024</header>
+                        <div class="tools">
+                            <a class="fa fa-repeat btn-color box-refresh" href="javascript:;"></a>
+                            <a class="t-collapse btn-color fa fa-chevron-down" href="javascript:;"></a>
+                            <a class="t-close btn-color fa fa-times" href="javascript:;"></a>
+                        </div>
+                    </div>
+                    <div class="card-body no-padding height-9">
+                        <div class="row">
+                            <table
+                                class="table table-striped table-bordered table-hover"
+                                <thead>
+                                <tr>
+                                    <th> Code </th>
+                                    <th> Name </th>
+                                    <th> Credits </th>
+                                </tr>
+                                </thead>
+
+                                <tbody>
+                                    <td>ZOO-1010</td>
+                                    <td class="left">Zoology</td>
+                                    <td>3</td>
+                                </tbody>
+
+                                <tbody>
+                                    <td>WOH-2022</td>
+                                    <td class="left">Hist. of World Civil. 1789</td>
+                                    <td>3</td>
+                                </tbody>
+
+                                <tbody>
+                                    <td>COP-2800</td>
+                                    <td class="left">Java</td>
+                                    <td>3</td>
+                                </tbody>
+
+                                <tbody>
+                                    <td>MAD-2104</td>
+                                    <td class="left">Discrete Mathematics</td>
+                                    <td>3</td>
+                                </tbody>
+
+                            </table>
+                                    </div><div class="slimScrollBar" style="background: rgb(158, 165, 171); width: 5px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 210.39px;"></div><div class="slimScrollRail" style="width: 5px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+                            <div class="card  card-box">
+                                <div class="card-head">
+                                    <header>Fall 2024</header>
+                                    <div class="tools">
+                                        <a class="fa fa-repeat btn-color box-refresh" href="javascript:;"></a>
+                                        <a class="t-collapse btn-color fa fa-chevron-down" href="javascript:;"></a>
+                                        <a class="t-close btn-color fa fa-times" href="javascript:;"></a>
+                                    </div>
+                                </div>
+                                <div class="card-body no-padding height-9">
+                                    <div class="row">
+                                        <table
+                                            class="table table-striped table-bordered table-hover"
+                                        <thead>
+                                        <tr>
+                                            <th> Code </th>
+                                            <th> Name </th>
+                                            <th> Credits </th>
+                                        </tr>
+                                        </thead>
+
+                                        <tbody>
+                                        <td>ZOO-1010</td>
+                                        <td class="left">Zoology</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        <tbody>
+                                        <td>WOH-2022</td>
+                                        <td class="left">Hist. of World Civil. 1789</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        <tbody>
+                                        <td>COP-2800</td>
+                                        <td class="left">Java</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        <tbody>
+                                        <td>MAD-2104</td>
+                                        <td class="left">Discrete Mathematics</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        </table>
+                                    </div><div class="slimScrollBar" style="background: rgb(158, 165, 171); width: 5px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 210.39px;"></div><div class="slimScrollRail" style="width: 5px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+                            <div class="card  card-box">
+                                <div class="card-head">
+                                    <header>Spring 2025</header>
+                                    <div class="tools">
+                                        <a class="fa fa-repeat btn-color box-refresh" href="javascript:;"></a>
+                                        <a class="t-collapse btn-color fa fa-chevron-down" href="javascript:;"></a>
+                                        <a class="t-close btn-color fa fa-times" href="javascript:;"></a>
+                                    </div>
+                                </div>
+                                <div class="card-body no-padding height-9">
+                                    <div class="row">
+                                        <table
+                                            class="table table-striped table-bordered table-hover"
+                                        <thead>
+                                        <tr>
+                                            <th> Code </th>
+                                            <th> Name </th>
+                                            <th> Credits </th>
+                                        </tr>
+                                        </thead>
+
+                                        <tbody>
+                                        <td>ZOO-1010</td>
+                                        <td class="left">Zoology</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        <tbody>
+                                        <td>WOH-2022</td>
+                                        <td class="left">Hist. of World Civil. 1789</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        <tbody>
+                                        <td>COP-2800</td>
+                                        <td class="left">Java</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        <tbody>
+                                        <td>MAD-2104</td>
+                                        <td class="left">Discrete Mathematics</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        </table>
+                                    </div><div class="slimScrollBar" style="background: rgb(158, 165, 171); width: 5px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 210.39px;"></div><div class="slimScrollRail" style="width: 5px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+                            <div class="card  card-box">
+                                <div class="card-head">
+                                    <header>Summer 2025</header>
+                                    <div class="tools">
+                                        <a class="fa fa-repeat btn-color box-refresh" href="javascript:;"></a>
+                                        <a class="t-collapse btn-color fa fa-chevron-down" href="javascript:;"></a>
+                                        <a class="t-close btn-color fa fa-times" href="javascript:;"></a>
+                                    </div>
+                                </div>
+                                <div class="card-body no-padding height-9">
+                                    <div class="row">
+                                        <table
+                                            class="table table-striped table-bordered table-hover"
+                                        <thead>
+                                        <tr>
+                                            <th> Code </th>
+                                            <th> Name </th>
+                                            <th> Credits </th>
+                                        </tr>
+                                        </thead>
+
+                                        <tbody>
+                                        <td>ZOO-1010</td>
+                                        <td class="left">Zoology</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        <tbody>
+                                        <td>WOH-2022</td>
+                                        <td class="left">Hist. of World Civil. 1789</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        <tbody>
+                                        <td>COP-2800</td>
+                                        <td class="left">Java</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        <tbody>
+                                        <td>MAD-2104</td>
+                                        <td class="left">Discrete Mathematics</td>
+                                        <td>3</td>
+                                        </tbody>
+
+                                        </table>
+                                    </div><div class="slimScrollBar" style="background: rgb(158, 165, 171); width: 5px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 210.39px;"></div><div class="slimScrollRail" style="width: 5px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end page content -->
+@endsection
+
